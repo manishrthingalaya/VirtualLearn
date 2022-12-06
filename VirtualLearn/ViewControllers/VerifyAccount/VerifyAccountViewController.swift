8e6d9b0c63ddda04c686aec36acdd0ff764fe234
sushruth <sushruth.hg@robosoftin.com>
2022-12-06 13:25:44 +0530

    verify controller commit

---
+++ VirtualLearn/ViewControllers/VerifyAccount/VerifyAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VerifyAccountViewController.swift
+//  VirtualLearn
+//
+//  Created by Sushruth H G on 06/12/22.
+//
+
+import UIKit
+
+class VerifyAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
