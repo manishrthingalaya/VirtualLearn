9b214d1bf79ecc11cb0ab5e50b1fd33e1a50c700
Anushree JC <anushree.jc@robosoftin.com>
2022-12-06 20:42:13 +0530

    commit

--- VirtualLearn/View/ButtonView.swift
+++ VirtualLearn/View/ButtonView.swift
@@ -14,4 +14,12 @@ extension UIButton {
     func addingRadius() {
         self.layer.cornerRadius = 6
     }
+    
+    func addingBorderColor() {
+        self.layer.cornerRadius = 8
+        self.layer.borderWidth = 2;
+       self.layer.borderColor = CGColor(red: 238/255, green: 92/255, blue: 77/255, alpha: 1)
+    }
+    
+    
 }
--- VirtualLearn/ViewControllers/Landing/LandingViewController.swift
+++ VirtualLearn/ViewControllers/Landing/LandingViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class LandingViewController: UIViewController {
-
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        registerButton.addingBorderColor()
+        
         // Do any additional setup after loading the view.
     }
     
