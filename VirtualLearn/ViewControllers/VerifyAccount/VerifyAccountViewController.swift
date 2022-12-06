a9f798d79d3d744bed0d4fe468b84c2a2df206f9
sushruth <sushruth.hg@robosoftin.com>
2022-12-06 16:47:26 +0530

    1stDayVerifyCommit

--- VirtualLearn/ViewControllers/VerifyAccount/VerifyAccountViewController.swift
+++ VirtualLearn/ViewControllers/VerifyAccount/VerifyAccountViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class VerifyAccountViewController: UIViewController {
 
+    @IBOutlet weak var firstDigitUnderView: UIView!
+    @IBOutlet weak var secondDigitUnderView: UIView!
+    @IBOutlet weak var thirdDigitUnderView: UIView!
+    @IBOutlet weak var fourthDigitUnderView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
